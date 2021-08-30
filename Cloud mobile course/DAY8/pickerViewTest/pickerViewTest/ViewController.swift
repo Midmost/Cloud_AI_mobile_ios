02365c662caaa938afc547fbc9d6f63789965a06
Midmost <csoheon@naver.com>
2021-08-30 15:09:04 +0900

    pickerView

--- Cloud mobile course/DAY8/pickerViewTest/pickerViewTest/ViewController.swift
+++ Cloud mobile course/DAY8/pickerViewTest/pickerViewTest/ViewController.swift
@@ -7,17 +7,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
-    let datasource = PickerViewDatasource
+    let bts = ["jin", "RM", "sugar", "v", "jhop", "jukuk", "whoever"]
+    
+    let datasource = PickerViewDatasource()
     @IBOutlet weak var pickerView: UIPickerView!
+    @IBOutlet weak var label: UILabel!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        pickerView.dataSource = datasource
-     
+        pickerView.dataSource = self
+        pickerView.delegate = self
     }
+    
+    
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        //return 4
+        return bts.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+       // return "\(row)번째 row "
+        return bts[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        label.text = bts[row]
+    }
+    
+//
+    
+    
 
 
 }
--- Cloud mobile course/DAY8/pickerViewTest/pickerViewTest/pickerViewDatasource.swift
+++ Cloud mobile course/DAY8/pickerViewTest/pickerViewTest/pickerViewDatasource.swift
@@ -11,15 +11,27 @@ import UIKit
 //데이터 소스로 쓰려면?
 //프로토콜의 데이터소스를 적용해야해
 
-class PickerViewDatasource: NSObject, UIPickerViewDataSource {
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        <#code#>
-    }
+class PickerViewDatasource: NSObject{
     
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        <#code#>
-    }
-    
+//
+//    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+//        return 1
+//    }
+//
+//    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+//        //return 4
+//        return bts.count
+//    }
+//
+//    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+//       // return "\(row)번째 row "
+//        return bts[row]
+//    }
+//
+//    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+//        label.text = bts[row]
+//    }
+//
 
 }
 
